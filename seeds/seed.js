af70fa6822c578369047eb61b3d5b296c474a872
Henri <ecch9210@gmail.com>
2021-07-18 22:01:49 -0500

    Helper and seed.js finished

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,20 @@
 const sequelize = require('../config/connection');
-const { Example } = require('../models');
+const { User,Comment,Post } = require('../models');
 
-const exampleData = require('./exampleData.json');
+const commentData = require('./commentData.json');
+const postData = require('./postData.json'); 
+const userData = require('./userData.json'); 
 
 const seedDatabase = async () => {
+  try{
   await sequelize.sync({ force: true });
 
-  await Example.bulkCreate(exampleData);
-
+  await User.bulkCreate(userData);
+  await Post.bulkCreate(postData);
+  await Comment.bulkCreate(commentData);
+} catch (err) {
+    console.log(err);
+  }
   process.exit(0);
 };
 
