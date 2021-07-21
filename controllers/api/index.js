3922e6a29ccdecd813281275a1e31807dd20b62b
Henri <ecch9210@gmail.com>
2021-07-21 14:20:29 -0500

    Working on Homeroutes, finished index api file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentsRoutes = require('./commentsRoutes');
 
-
-
+router.use('/postRoutes', postRoutes);
 router.use('/userRoutes', userRoutes);
+router.use('/commentsRoutes', commentsRoutes);
 
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,43 @@
 const router = require('express').Router();
-const { Reservations,Classes,User } = require('../models');
+const { Comment,Post,User } = require('../models');
 
+//Homepage
+// router.get('/', async (req, res) => {
+//   try {
+//     res.render('homepage', {
+//       loggedIn: req.session.logged_in,
+//       home: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 router.get('/', async (req, res) => {
   try {
+    // Find the logged in user based on the session ID
+    const postData = await Post.findAll({
+      attributes: {
+        exclude: ["user_id", "createdAt"],
+      },
+      include: [
+        {
+          model: Post,
+          attributes: {
+            exclude: ["id"],
+          },
+        },
+        
+        ]
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts)
+    
     res.render('homepage', {
+      posts,
       loggedIn: req.session.logged_in,
-      home: true
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
