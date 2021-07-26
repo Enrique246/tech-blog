2c2660d72798aea620950de06ec72956bdd889d7
Henri <ecch9210@gmail.com>
2021-07-26 12:35:42 -0500

    Fising comment viewing front-end

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,10 +2,18 @@
 // Week 14 Exercise 14
 module.exports = {
         format_date: (date) => {
+          console.log(date)
+          // return `${Date.getMonh() + 1}/${Date.getDate()}/${Date.getFullYear()}`;
+
           return `${new Date(date).getDate()}/${
             new Date(date).getMonth() + 1
-          }/${new Date(date).getFullYear()} at ${date.toLocaleTimeString()}`;
+          }/${new Date(date).getFullYear()} `;
+          // at ${date.toLocaleTimeString()}
+       
         },
+        // format_date: date => {
+        //   return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        // }
         format_plural: (word, amount) => {
           if (amount !== 1) {
             return `${word}s`;
@@ -13,4 +21,5 @@ module.exports = {
       
           return word;
       }
+     
 };
