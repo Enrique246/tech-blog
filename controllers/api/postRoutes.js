17741268819a04b51a66929b03993a8453d4b789
Henri <ecch9210@gmail.com>
2021-07-25 23:21:27 -0500

    Fixed bugs of comments model and homeroutes to be able to render one post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -23,45 +23,45 @@ router.get("/", async (req, res) => {
   });
 
 //Get post by id
-  router.get('/:id', (req, res) => {
-    Post.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        'body'
-      ],
-      include: [
-        // include the Comment model here:
-        {
-          model: User,
-          attributes: ['username']
-        },
-        {
-          model: Comment,
-          attributes: ['id', 'text_comment', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username',]
-          }
-        }
-      ]
-    })
-      .then(postData => {
-        if (!postData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(postData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+  // router.get('/:id', (req, res) => {
+  //   Post.findOne({
+  //     where: {
+  //       id: req.params.id
+  //     },
+  //     attributes: [
+  //       'id',
+  //       'title',
+  //       'created_at',
+  //       'body'
+  //     ],
+  //     include: [
+  //       // include the Comment model here:
+  //       {
+  //         model: User,
+  //         attributes: ['username']
+  //       },
+  //       {
+  //         model: Comment,
+  //         attributes: ['id', 'text_comment', 'post_id', 'user_id', 'created_at'],
+  //         include: {
+  //           model: User,
+  //           attributes: ['username',]
+  //         }
+  //       }
+  //     ]
+  //   })
+  //     .then(postData => {
+  //       if (!postData) {
+  //         res.status(404).json({ message: 'No post found with this id' });
+  //         return;
+  //       }
+  //       res.json(postData);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //       res.status(500).json(err);
+  //     });
+  // });
   
   // Creating post
   router.post("/", async (req, res) => {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,6 +40,54 @@ router.get('/', async (req, res) => {
   }
 });
 
+//Get post by id
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'createdAt',
+      'body'
+    ],
+    include: [
+      // include the Comment model here:
+      {
+        model: User,
+        attributes: ['name']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'text_comment', 'post_id', 'user_id', 'createdAt'],
+        include: {
+          model: User,
+          attributes: ['name',]
+        }
+      }
+    ]
+  })
+
+  
+    .then(postData => {
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      console.log(postData)
+      res.render('onepost', {
+        posts:postData,
+        loggedIn: req.session.logged_in,
+        home: true
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 //Login
 router.get('/login', (req, res) => {
   res.render('login',{
